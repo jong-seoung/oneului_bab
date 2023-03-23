fcdf9ed6ed857692265ae0384245bd063a429fa1
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2023-03-23 16:39:37 +0900

    회원가입시 이메일 인증번호 전송 및 인증 기능 추가, 리다이렉트시 기존 정보 유지
    회원가입시 인증번호 보내기로 인증번호를 보낸후 인증번호 입력하고 가입하기를 누르면 가입이 되도록 설정 - db에 저장하는 부분과 gmail 비밀번호는 주석처리
    회원정보를 잘못입력하고 가입하기나 인증번호를 보내면 에러메시지와 함께 리다이렉트시 기존 입력정보를 유지한 상태로 리다이렉트

--- oneului_bab/user/views.py
+++ oneului_bab/user/views.py
@@ -7,6 +7,18 @@ from django.contrib import messages
 from user.forms import CustomLoginForm
 from .models import User
 from django.shortcuts import render, redirect
+import smtplib
+from email.mime.text import MIMEText
+
+#########################
+import json
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+secrets_file = os.path.join(BASE_DIR, 'secrets.json')
+##########################
 
 class LoginView(LoginView):           # 로그인
     authentication_form = CustomLoginForm
@@ -43,12 +55,12 @@ class LogoutView(LogoutView):
         return self.next_page
 
 class FindIDView(View):
-    template_name = 'user/Find_ID.html'
+    template_name = 'user/find_ID.html'
     def get(self, request):
         return render(request, 'user/find_ID.html')
     
 class FindPasswordView(View):
-    template_name = 'user/Find_Password.html'
+    template_name = 'user/find_Password.html'
     def get(self, request):
         return render(request, 'user/find_Password.html')
     
@@ -57,41 +69,104 @@ class JoinView(View):
     def get(self, request):
         return render(request, 'user/join.html')
 
-    def post(self, request):
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
-        name = request.POST['name']
-        nickname = request.POST['nickname']
-        birthdate_year = request.POST['birthdate_year']
-        birthdate_month = request.POST['birthdate_month']
-        birthdate_days = request.POST['birthdate_days']
-        birthdate = birthdate_year + "-" + birthdate_month + "-" + birthdate_days
+    def send_email_code(self, request, email):
+        import random
+        code = random.randint(100000, 999999)
+        message = f"이메일 인증 코드 입니다. : {code}"
+        msg = MIMEText(message)
+        msg['Subject'] = '오늘의 밥 : 회원가입 인증 코드'
+        msg['From'] = 'jjong015189@gmail.com'
+        msg['To'] = email
+
+        with open(secrets_file) as f:
+            secrets = json.load(f)
+
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.starttls()
+        s.login('jjong015189@gmail.com', secrets['GMAIL_KEY'])
+        s.sendmail('jjong015189@gmail.com', [email], msg.as_string())
+        s.quit()
+        request.session['email_verification_code'] = code
+        return code
+
+    def check_email_code(self, request):
         emailcode = request.POST['emailcode']
-        info = {'email': email, 'password': password, 'confirm_password': confirm_password, 'name': name, 'nickname': nickname, 'birthdate_year': birthdate_year, 'birthdate_month': birthdate_month, 'birthdate_days': birthdate_days, 'emailcode': emailcode}
-        
-        if password != confirm_password:
-            messages.error(request, '비밀번호와 비밀번호 확인이 일치하지 않습니다.')
-            return HttpResponse(render(request, 'user/join.html', info))
-        
-        if User.objects.filter(email=email).exists():
-            messages.error(request, '이미 존재하는 이메일입니다.')
-            return HttpResponse(render(request, 'user/join.html', info))
-        
-        if emailcode == "":
-            messages.error(request, '이메일 인증을 해주세요')
-            return HttpResponse(render(request, 'user/join.html', info))
-        elif emailcode != "o":
-            messages.error(request, '인증코드가 올바르지않습니다.')
-            return HttpResponse(render(request, 'user/join.html', info))
-        
-        
-        # user = User.objects.create_user(email=email, password=password, name=name, nickname=nickname, birth_date=birthdate)
+        if 'email_verification_code' in request.session:
+            if int(emailcode) == request.session['email_verification_code']:
+                return True
+        return False
+    
+    def post(self, request):
+        if request.POST.get('submit') == 'email-verify':
+            print(1)
+            email = request.POST['email']
+            password = request.POST['password']
+            confirm_password = request.POST['confirm_password']
+            name = request.POST['name']
+            nickname = request.POST['nickname']
+            birthdate_year = request.POST['birthdate_year']
+            birthdate_month = request.POST['birthdate_month']
+            birthdate_days = request.POST['birthdate_days']
+            birthdate = birthdate_year + "-" + birthdate_month + "-" + birthdate_days
+            emailcode = request.POST['emailcode']
+            info = {'email': email, 'password': password, 'confirm_password': confirm_password, 'name': name, 'nickname': nickname, 'birthdate_year': birthdate_year, 'birthdate_month': birthdate_month, 'birthdate_days': birthdate_days, 'emailcode': emailcode}
+            
+            if password != confirm_password:
+                messages.error(request, '비밀번호와 비밀번호 확인이 일치하지 않습니다.')
+                return HttpResponse(render(request, 'user/join.html', info))
+            
+            if User.objects.filter(email=email).exists():
+                messages.error(request, '이미 존재하는 이메일입니다.')
+                return HttpResponse(render(request, 'user/join.html', info))
+            
+            if User.objects.filter(nickname=nickname).exists():
+                messages.error(request, '이미 존재하는 닉네임입니다.')
+                return HttpResponse(render(request, 'user/join.html', info))
 
-        # user.save()
+            self.send_email_code(request, email)
+            
+            messages.success(request, '인증번호가 전송되었습니다.')
+            return HttpResponse(render(request, 'user/join.html', info))
+        elif request.POST.get('submit') == 'join':
+            print(2)
+            email = request.POST['email']
+            password = request.POST['password']
+            confirm_password = request.POST['confirm_password']
+            name = request.POST['name']
+            nickname = request.POST['nickname']
+            birthdate_year = request.POST['birthdate_year']
+            birthdate_month = request.POST['birthdate_month']
+            birthdate_days = request.POST['birthdate_days']
+            birthdate = birthdate_year + "-" + birthdate_month + "-" + birthdate_days
+            emailcode = request.POST['emailcode']
+            info = {'email': email, 'password': password, 'confirm_password': confirm_password, 'name': name, 'nickname': nickname, 'birthdate_year': birthdate_year, 'birthdate_month': birthdate_month, 'birthdate_days': birthdate_days, 'emailcode': emailcode}
+            
+            if password != confirm_password:
+                messages.error(request, '비밀번호와 비밀번호 확인이 일치하지 않습니다.')
+                return HttpResponse(render(request, 'user/join.html', info))
+            
+            if User.objects.filter(email=email).exists():
+                messages.error(request, '이미 존재하는 이메일입니다.')
+                return HttpResponse(render(request, 'user/join.html', info))
+            
+            if User.objects.filter(nickname=nickname).exists():
+                messages.error(request, '이미 존재하는 닉네임입니다.')
+                return HttpResponse(render(request, 'user/join.html', info))
+            
+            if emailcode == "":
+                messages.error(request, '이메일 인증을 해주세요')
+                return HttpResponse(render(request, 'user/join.html', info))
+            else:
+                if self.check_email_code(request) == True:
+                    messages.success(request, '회원가입이 완료되었습니다.')
+                elif self.check_email_code(request) == False:
+                    messages.success(request, '인증번호가 잘못되었습니다. 다시 확인해주세요.')
+                    return HttpResponse(render(request, 'user/join.html', info))
 
-        # user = authenticate(email=email, password=password)
-        # login(request, user)
+            # user = User.objects.create_user(email=email, password=password, name=name, nickname=nickname, birth_date=birthdate)
 
-        messages.success(request, '회원가입이 완료되었습니다.')
+            # user.save()
+
+            # user = authenticate(email=email, password=password)
+            # login(request, user)
         return redirect('loginview')
